99e8cbb1428603f0dab9979ba9bf1548a4f621d7
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-10-17 16:10:12 -0300

    Added poems

--- botsuki.rb
+++ botsuki.rb
@@ -5,6 +5,7 @@ require 'yaml'
 
 $config = YAML.load_file('config.yaml')
 $flavours = $config['flavoursFile']
+$poems = YAML.load_file('poems.yaml')
 
 $bsuki = Discordrb::Commands::CommandBot.new(
   token: $config['token'],
@@ -54,6 +55,29 @@ do |event|
   do |embed|
     embed.image = Discordrb::Webhooks::EmbedImage.new(
       url: 'https://cdn.discordapp.com/attachments/369241481095020546/369551522583019540/BeyXDzd.png')
+  end
+end
+
+$poems.keys.each \
+do |poem|
+
+  $bsuki.command poem.to_sym \
+  do |event|
+
+    event.channel.send_embed \
+    do |embed|
+
+      embed_raw = $poems[poem]['embed']
+      embed.description = embed_raw['description']
+
+      if embed_raw['author']
+        embed_raw = embed_raw['author']
+        embed.author = Discordrb::Webhooks::EmbedAuthor.new(
+          name: embed_raw['name'],
+          url: embed_raw['url'],
+          icon_url: embed_raw['icon_url'] )
+      end
+    end
   end
 end
 
